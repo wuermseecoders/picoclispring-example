1abe1fa3e583763d03e179cf611a11f96a47c3ea
Björn Höfling <bjoern.hoefling@camptocamp.com>
2021-12-01 20:48:29 +0100

    From Spring starter.

---
+++ src/main/java/com/camptocamp/bhoefling/picoclispring/PicoclispringApplication.java
@@ -0,0 +1,13 @@
+package com.camptocamp.bhoefling.picoclispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicoclispringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicoclispringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/camptocamp/bhoefling/picoclispring/PicoclispringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camptocamp.bhoefling.picoclispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicoclispringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
