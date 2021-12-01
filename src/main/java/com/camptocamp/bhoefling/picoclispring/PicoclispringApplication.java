f6e9d2ce66feba2d4eb874037c07958e2d249d9e
Björn Höfling <bjoern.hoefling@camptocamp.com>
2021-12-01 20:59:00 +0100

    Format with spaces.

--- src/main/java/com/camptocamp/bhoefling/picoclispring/PicoclispringApplication.java
+++ src/main/java/com/camptocamp/bhoefling/picoclispring/PicoclispringApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PicoclispringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PicoclispringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PicoclispringApplication.class, args);
+    }
 
 }
